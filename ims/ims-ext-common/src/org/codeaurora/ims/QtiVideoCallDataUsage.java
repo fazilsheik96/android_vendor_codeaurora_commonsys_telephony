LineageOS/android_vendor_codeaurora_telephony

IMS: Add Motorola specific API for VT
 * The binder object is incompatible with original QCOM, so set
   ro.qcom.ims.use_moto_vt_ext=true to enable.
 * The IMS stack will crash when making a VT call without these
   extensions, and the outcoming call cannot be hung up unless restart.

Change-Id: I557daf261472fccbd7792934d4a377d5cbb2f252

ims/ims-ext-common/src/org/codeaurora/ims/QtiVideoCallDataUsage.java
@@ -31,6 +31,7 @@ package org.codeaurora.ims;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.os.SystemProperties;
 
 /**
  * Parcelable object to handle Video Call DataUsage information
@@ -45,31 +46,82 @@ public class QtiVideoCallDataUsage implements Parcelable {
     public static final int DATA_USAGE_INVALID_VALUE = -1;
     private static final String[] TEXT = {"LteDataUsage = "," WlanDataUsage = "};
 
+    // Motorola specific
+    public static final int DATA_USAGE_LTE_TX = 0;
+    public static final int DATA_USAGE_LTE_RX = 1;
+    public static final int DATA_USAGE_WLAN_TX = 2;
+    public static final int DATA_USAGE_WLAN_RX = 3;
+    private static final String[] TEXT_MOTO = {
+        "LteTxDataUsage = ", " LteRxDataUsage = ", " WlanTxDataUsage = ", " WlanRxDataUsage = "
+    };
+    private static final String MOTO_VT_PROP = "ro.qcom.ims.use_moto_vt_ext";
+    private final boolean mUseMotoExt;
+
     public QtiVideoCallDataUsage(long[] dUsage) {
         if (dUsage == null || dUsage.length == 0 ) {
             throw new RuntimeException();
         }
         mDataUsage = dUsage;
+        mUseMotoExt = SystemProperties.getBoolean(MOTO_VT_PROP, false);
     }
 
     public QtiVideoCallDataUsage(Parcel in) {
         readFromParcel(in);
+        mUseMotoExt = SystemProperties.getBoolean(MOTO_VT_PROP, false);
     }
 
     /*
      * This method returns LTE Data Usage
      */
     public long getLteDataUsage() {
-        return mDataUsage.length > DATA_USAGE_LTE ? mDataUsage[DATA_USAGE_LTE] :
-                DATA_USAGE_INVALID_VALUE;
+        if (mUseMotoExt) {
+            return mDataUsage.length > DATA_USAGE_LTE_RX
+                    ? mDataUsage[DATA_USAGE_LTE_TX] + mDataUsage[DATA_USAGE_LTE_RX]
+                    : DATA_USAGE_INVALID_VALUE;
+        } else {
+            return mDataUsage.length > DATA_USAGE_LTE
+                    ? mDataUsage[DATA_USAGE_LTE]
+                    : DATA_USAGE_INVALID_VALUE;
+        }
+    }
+
+    public long getLteTxDataUsage() {
+        return (mUseMotoExt && mDataUsage.length > DATA_USAGE_LTE_TX)
+                ? mDataUsage[DATA_USAGE_LTE_TX]
+                : DATA_USAGE_INVALID_VALUE;
+    }
+
+    public long getLteRxDataUsage() {
+        return (mUseMotoExt && mDataUsage.length > DATA_USAGE_LTE_RX)
+                ? mDataUsage[DATA_USAGE_LTE_RX]
+                : DATA_USAGE_INVALID_VALUE;
     }
 
     /*
      * This method returns WLAN Data Usage
      */
     public long getWlanDataUsage() {
-        return mDataUsage.length > DATA_USAGE_WLAN ? mDataUsage[DATA_USAGE_WLAN] :
-                DATA_USAGE_INVALID_VALUE;
+        if (mUseMotoExt) {
+            return mDataUsage.length > DATA_USAGE_WLAN_RX
+                    ? mDataUsage[DATA_USAGE_WLAN_TX] + mDataUsage[DATA_USAGE_WLAN_RX]
+                    : DATA_USAGE_INVALID_VALUE;
+        } else {
+            return mDataUsage.length > DATA_USAGE_WLAN
+                    ? mDataUsage[DATA_USAGE_WLAN]
+                    : DATA_USAGE_INVALID_VALUE;
+        }
+    }
+
+    public long getWlanTxDataUsage() {
+        return (mUseMotoExt && mDataUsage.length > DATA_USAGE_WLAN_TX)
+                ? mDataUsage[DATA_USAGE_WLAN_TX]
+                : DATA_USAGE_INVALID_VALUE;
+    }
+
+    public long getWlanRxDataUsage() {
+        return (mUseMotoExt && mDataUsage.length > DATA_USAGE_WLAN_RX)
+                ? mDataUsage[DATA_USAGE_WLAN_RX]
+                : DATA_USAGE_INVALID_VALUE;
     }
 
     @Override
@@ -103,8 +155,9 @@ public class QtiVideoCallDataUsage implements Parcelable {
     public String toString() {
         if(mDataUsage != null) {
             String msg = "";
+            String[] text = mUseMotoExt ? TEXT_MOTO : TEXT;
             for (int i = 0; i < mDataUsage.length; i++) {
-                  msg += TEXT[i] + mDataUsage[i];
+                msg += text[i] + mDataUsage[i];
             }
             return msg;
         }

fazilsheik96/android_vendor_codeaurora_commonsys_telephony

 * The data format is incompatible with original QCOM, so set

ims/ims-ext-common/src/org/codeaurora/ims/QtiVideoCallDataUsage.java
@@ -35,6 +35,7 @@ package org.codeaurora.ims;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.os.SystemProperties;
 
 /**
  * Parcelable object to handle Video Call DataUsage information
@@ -51,31 +52,82 @@ public class QtiVideoCallDataUsage implements Parcelable {
     private static final String[] TEXT =
             {"WwanDataUsage = "," WlanDataUsage = ", " CIWlanDataUsage = "};
 
+    // Motorola specific
+    public static final int DATA_USAGE_WWAN_TX = 0;
+    public static final int DATA_USAGE_WWAN_RX = 1;
+    public static final int DATA_USAGE_WLAN_TX = 2;
+    public static final int DATA_USAGE_WLAN_RX = 3;
+    private static final String[] TEXT_MOTO = {
+        "WwanTxDataUsage = ", " WwanRxDataUsage = ", " WlanTxDataUsage = ", " WlanRxDataUsage = "
+    };
+    private static final String MOTO_VT_PROP = "ro.qcom.ims.use_moto_vt_ext";
+    private final boolean mUseMotoExt;
+
     public QtiVideoCallDataUsage(long[] dUsage) {
         if (dUsage == null || dUsage.length == 0 ) {
             throw new RuntimeException();
         }
         mDataUsage = dUsage;
+        mUseMotoExt = SystemProperties.getBoolean(MOTO_VT_PROP, false);
     }
 
     public QtiVideoCallDataUsage(Parcel in) {
         readFromParcel(in);
+        mUseMotoExt = SystemProperties.getBoolean(MOTO_VT_PROP, false);
     }
 
     /*
      * This method returns WWAN Data Usage
      */
     public long getWwanDataUsage() {
-        return mDataUsage.length > DATA_USAGE_WWAN ? mDataUsage[DATA_USAGE_WWAN] :
-                DATA_USAGE_INVALID_VALUE;
+        if (mUseMotoExt) {
+            return mDataUsage.length > DATA_USAGE_WWAN_RX
+                    ? mDataUsage[DATA_USAGE_WWAN_TX] + mDataUsage[DATA_USAGE_WWAN_RX]
+                    : DATA_USAGE_INVALID_VALUE;
+        } else {
+            return mDataUsage.length > DATA_USAGE_WWAN
+                    ? mDataUsage[DATA_USAGE_WWAN]
+                    : DATA_USAGE_INVALID_VALUE;
+        }
+    }
+
+    public long getWwanTxDataUsage() {
+        return (mUseMotoExt && mDataUsage.length > DATA_USAGE_WWAN_TX)
+                ? mDataUsage[DATA_USAGE_WWAN_TX]
+                : DATA_USAGE_INVALID_VALUE;
+    }
+
+    public long getWwanRxDataUsage() {
+        return (mUseMotoExt && mDataUsage.length > DATA_USAGE_WWAN_RX)
+                ? mDataUsage[DATA_USAGE_WWAN_RX]
+                : DATA_USAGE_INVALID_VALUE;
     }
 
     /*
      * This method returns WLAN Data Usage
      */
     public long getWlanDataUsage() {
-        return mDataUsage.length > DATA_USAGE_WLAN ? mDataUsage[DATA_USAGE_WLAN] :
-                DATA_USAGE_INVALID_VALUE;
+        if (mUseMotoExt) {
+            return mDataUsage.length > DATA_USAGE_WLAN_RX
+                    ? mDataUsage[DATA_USAGE_WLAN_TX] + mDataUsage[DATA_USAGE_WLAN_RX]
+                    : DATA_USAGE_INVALID_VALUE;
+        } else {
+            return mDataUsage.length > DATA_USAGE_WLAN
+                    ? mDataUsage[DATA_USAGE_WLAN]
+                    : DATA_USAGE_INVALID_VALUE;
+        }
+    }
+
+    public long getWlanTxDataUsage() {
+        return (mUseMotoExt && mDataUsage.length > DATA_USAGE_WLAN_TX)
+                ? mDataUsage[DATA_USAGE_WLAN_TX]
+                : DATA_USAGE_INVALID_VALUE;
+    }
+
+    public long getWlanRxDataUsage() {
+        return (mUseMotoExt && mDataUsage.length > DATA_USAGE_WLAN_RX)
+                ? mDataUsage[DATA_USAGE_WLAN_RX]
+                : DATA_USAGE_INVALID_VALUE;
     }
 
     /*
@@ -117,8 +169,9 @@ public class QtiVideoCallDataUsage implements Parcelable {
     public String toString() {
         if(mDataUsage != null) {
             String msg = "";
+            String[] text = mUseMotoExt ? TEXT_MOTO : TEXT;
             for (int i = 0; i < mDataUsage.length; i++) {
-                  msg += TEXT[i] + mDataUsage[i];
+                msg += text[i] + mDataUsage[i];
             }
             return msg;
         }